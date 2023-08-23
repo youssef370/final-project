5ab9f8a563dfc503a85b798d316d07b065e9b523
Youssef Emrani <youssef.emrani@gmail.com>
2023-08-23 19:40:12 +0100

    Fixed bugs

--- website/views.py
+++ website/views.py
@@ -7,11 +7,15 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template('home.html', user=current_user)
+    db_posts = Post.query.all()
+    posts = [post for post in db_posts]
+    return render_template('home.html', user=current_user, posts=posts)
 
 @views.route('/new-post', methods=['GET', 'POST'])
 @login_required
 def new_post():
     if request.method == 'POST':
-        pass
+        post = request.form.get('post-text')
+        post_title = request.form.get('post-title')
+
     return render_template('new-post.html', user=current_user)
